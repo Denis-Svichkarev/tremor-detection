b868ee2dd7672ce87d119591a33a5db6faf9b2ed
Denis Svichkarev <denis.svichkarev@gmail.com>
2020-11-25 13:07:01 +0300

    Added iOS project

---
+++ iOS/TremorDetection/AppDelegate.swift
@@ -0,0 +1,19 @@
+//
+//  AppDelegate.swift
+//  TremorDetection
+//
+//  Created by Denis Svichkarev on 25.11.2020.
+//
+
+import UIKit
+
+@main
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        return true
+    }
+}
+
---
+++ iOS/TremorDetection/ViewController.swift
@@ -0,0 +1,18 @@
+//
+//  ViewController.swift
+//  TremorDetection
+//
+//  Created by Denis Svichkarev on 25.11.2020.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+
+}
+
